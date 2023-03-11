07cbd36b7305cbf29d259da03821a13f6bb3b7e6
murimichege <chegeeddie@gmail.com>
2023-03-12 01:27:35 +0300

    first commit

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+  <p>This is a react app</p>
+    </div>
+  )
+}
+
+export default App
